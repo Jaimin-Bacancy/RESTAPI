de357a766fbf3055276bbf35a8c6cc6cbfddca82
Jaimin-Bacancy <jaimin.parmar@bacancy.com>
2021-02-04 15:24:13 +0530

    updated file

--- configuration/controller/customercontroller.go
+++ configuration/controller/customercontroller.go
@@ -4,8 +4,14 @@ import (
 	"fmt"
 	"net/http"
 	"restapi/app/common"
+	"restapi/data/model"
 	"restapi/data/service"
 )
+
+var flaginsert bool
+var message string
+var flagupdate bool
+var flagdelete bool
 
 //CustomerIndexpageProcess is...
 func CustomerIndexpageProcess(w http.ResponseWriter, r *http.Request) {
@@ -17,11 +23,34 @@ func CustomerIndexpageProcess(w http.ResponseWriter, r *http.Request) {
 func CustomerDisplaypageProcess(w http.ResponseWriter, r *http.Request) {
 	tpl := common.GetTemplate()
 	customers := service.GetAllCustomer()
-	tpl.ExecuteTemplate(w, "display.html", customers)
+	var messageflag bool
+	if flaginsert {
+		flaginsert = false
+		message = "Data Inserted successfully"
+		messageflag = true
+	}
+
+	if flagupdate {
+		flagupdate = false
+		message = "Data Updated successfully"
+		messageflag = true
+	}
+
+	if flagdelete {
+		flagdelete = false
+		message = "Data is deleted successfully"
+		messageflag = true
+	}
+	tpl.ExecuteTemplate(w, "display.html", struct {
+		Customers  []model.Customer
+		Message    string
+		HasMessage bool
+	}{customers, message, messageflag})
 }
 
 //CustomerInsertProcess is...
 func CustomerInsertProcess(w http.ResponseWriter, r *http.Request) {
+	flaginsert = true
 	service.SaveOneCustomer(r)
 	http.Redirect(w, r, "/customer", 301)
 }
@@ -39,11 +68,13 @@ func CustomerEditpageProcess(w http.ResponseWriter, r *http.Request) {
 
 //CustomerUpdateProcess is...
 func CustomerUpdateProcess(w http.ResponseWriter, r *http.Request) {
+	flagupdate = true
 	service.UpdateOneCustomer(r)
 }
 
 //CustomerDeleteProcess is...
 func CustomerDeleteProcess(w http.ResponseWriter, r *http.Request) {
+	flagdelete = true
 	service.DeleteOneCustomer(r)
 }
 
